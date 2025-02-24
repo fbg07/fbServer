7bc560e88dc260735b16e1794f32913911e952e3
fbg07 <fbclient07@gmail.com>
2025-02-24 18:34:45 +0530

    Updated mongodb uri

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const DB_URL = process.env.DB || "mongodb+srv://fbuser:fb%4007@fbcluster.4zo1d.mongodb.net/?retryWrites=true&w=majority&appName=fbcluster/FBRental";
+const DB_URL = process.env.DB || "mongodb+srv://fbuser:fb%4007@fbcluster.4zo1d.mongodb.net/?retryWrites=true&w=majority&appName=fbcluster/FBRentals";
 
 
 const ConnectDB = async () => {
