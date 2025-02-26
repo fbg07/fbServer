ce98777e6e63175627b93c30ee425b83fd8659fd
fbg07 <fbclient07@gmail.com>
2025-02-26 11:04:51 +0530

    Update broker.js

--- src/Controllers/broker.js
+++ src/Controllers/broker.js
@@ -57,6 +57,7 @@ export const deletebroker = async (req, res) => {
 
     return res.status(200).json({
       message: "Broker deactivated successfully",
+      broker_id : id
     });
   } catch (error) {
     return res.status(500).json({
